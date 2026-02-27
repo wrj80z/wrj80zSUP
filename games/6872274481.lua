1df53185cbdd71755223c7f7064ec7b79a9de043
wrj80z <major.merritt14@icloud.com>
2026-02-27 13:04:13 -0500

    fixed rank n enchant nametags.

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -3859,28 +3859,19 @@ run(function()
 					Icon.Size = UDim2.fromOffset(30, 30)
 					Icon.Position = UDim2.fromOffset(size.X + 10, -4)
 					Icon.BackgroundTransparency = 1
-					local image = ''
-					local plr = playersService:GetPlayerFromCharacter(ent.Character)
-					bedwars.Client:Get(remotes.Ranks):CallServerAsync({plr.UserId}):andThen(function(data)
-						data = data[1]
-						local division = data.rankDivision
-						if division == nil then
-							division = 1
-						end
-						image = bedwars.RankMeta[division].image
-					end)
-					Icon.Image = image
+					Icon.Image = store.rank[ent.Player]:async() and bedwars.RankMeta[store.rank[ent.Player]:async()].image or ''
 					Icon.Parent = nametag
 				end
 			end)
 			task.spawn(function()
-				if Enchants.Enabled then
+				if Enchant.Enabled and ent.Player then
 					local Icon = Instance.new('ImageLabel')
 					Icon.Name = 'EnchantIcon'
 					Icon.Size = UDim2.fromOffset(30, 30)
-					Icon.Position = UDim2.fromOffset(size.X + 10, -8)
+					Icon.Position = UDim2.fromOffset(-30, -4)
 					Icon.BackgroundTransparency = 1
-					Icon.Image = ''
+					Icon.Image = store.enchants[ent.Player]:async() or ''
+					Icon.Parent = nametag
 				end
 			end)
 			Reference[ent] = nametag
