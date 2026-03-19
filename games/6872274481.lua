a6a7de52c6f0a3d6c3fed5c76e0205daa952d8da
wrj80z <major.merritt14@icloud.com>
2026-03-18 22:40:05 -0700

    Update 6872274481.lua

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -2842,98 +2842,86 @@ end)
 	
 run(function()
 	local NoFall
-	local Chance
-	local DamageAccuracy
-	local rand = Random.new()
+	local Mode
 	local rayParams = RaycastParams.new()
-	local groundHit = {}
-	local Modes
-	local Delay
-	function groundHit:FireServer(...)
-		bedwars.Client:Get(remotes.Ground):SendToServer(...)
-	end
+	local groundHit
+	task.spawn(function()
+		groundHit = bedwars.Client:Get(remotes.GroundHit).instance
+	end)
+	
 	NoFall = vape.Categories.Blatant:CreateModule({
 		Name = 'NoFall',
-		Alias = {'Fall','Damage'},
-		Tooltip = 'Prevents or reduces fall damage.',
 		Function = function(callback)
-			if not callback then return end
-			local tracked = 0
-			repeat
-				if not entitylib.isAlive then
-					tracked = 0
-					task.wait(0.2)
-					continue
-				end
-				local char = entitylib.character
-				local root = char.RootPart
-				local humanoid = char.Humanoid
-				if humanoid.FloorMaterial == Enum.Material.Air then
-					tracked = math.min(tracked, root.AssemblyLinearVelocity.Y)
-				else
-					tracked = 0
-				end
-				if tracked < -85 then
-					if rand:NextNumber(0, 100) <= Chance.Value then
-						local scaled = tracked * (DamageAccuracy.Value / 100)
-						groundHit:FireServer(nil,Vector3.new(0, scaled, 0),workspace:GetServerTimeNow())
-					end
-					tracked = 0
-				end
-				task.wait(0.03)
-			until not NoFall.Enabled
-		end
-	})
-	DamageAccuracy = NoFall:CreateSlider({
-		Name = 'Damage Accuracy',
-		Min = 0,
-		Max = 100,
-		Default = 0,
-		Visible = false,
-		Suffix = '%',
-		Decimal = 5,
-		Darker = true,
-	})
-	Chance = NoFall:CreateSlider({
-		Name = 'Chance',
-		Min = 0,
-		Max = 100,
-		Default = 100,
-		Visible = false,
-		Suffix = '%',
-		Darker = true,
-	})
-	Delay = NoFall:CreateSlider({
-		Name = 'Delay',
-		Min = 0,
-		Max = 1000,
-		Default = 250,
-		Visible = true,
-		Suffix = 'ms',
-		Darker = true,
-	})
+			if callback then
+				local tracked = 0
+				if Mode.Value == 'Gravity' then
+					local extraGravity = 0
+					NoFall:Clean(runService.PreSimulation:Connect(function(dt)
+						if entitylib.isAlive then
+							local root = entitylib.character.RootPart
+							if root.AssemblyLinearVelocity.Y < -85 then
+								rayParams.FilterDescendantsInstances = {lplr.Character, gameCamera}
+								rayParams.CollisionGroup = root.CollisionGroup
 	
-	Modes = NoFall:CreateDropdown({
-		Name = "Mode",
-		List = {'Packet','Delay'},
-		Default = 'Delay',
-		Function = function(v)
-			if v == 'Packet' then
-				Delay.Object.Visible = false
-				DamageAccuracy.Object.Visible = true
-				Chance.Object.Visible = true
-			elseif v == 'Delay' then
-				Delay.Object.Visible = true
-				DamageAccuracy.Object.Visible = false
-				Chance.Object.Visible = false
-			else
-				Delay.Object.Visible = false
-				DamageAccuracy.Object.Visible = false
-				Chance.Object.Visible = false
+								local rootSize = root.Size.Y / 2 + entitylib.character.HipHeight
+								local ray = workspace:Blockcast(root.CFrame, Vector3.new(3, 3, 3), Vector3.new(0, (tracked * 0.1) - rootSize, 0), rayParams)
+								if not ray then
+									root.AssemblyLinearVelocity = Vector3.new(root.AssemblyLinearVelocity.X, -86, root.AssemblyLinearVelocity.Z)
+									root.CFrame += Vector3.new(0, extraGravity * dt, 0)
+									extraGravity += -workspace.Gravity * dt
+								end
+							else
+								extraGravity = 0
+							end
+						end
+					end))
+				else
+					repeat
+						if entitylib.isAlive then
+							local root = entitylib.character.RootPart
+							tracked = entitylib.character.Humanoid.FloorMaterial == Enum.Material.Air and math.min(tracked, root.AssemblyLinearVelocity.Y) or 0
+	
+							if tracked < -85 then
+								if Mode.Value == 'Packet' then
+									groundHit:FireServer(nil, Vector3.new(0, tracked, 0), workspace:GetServerTimeNow())
+								else
+									rayParams.FilterDescendantsInstances = {lplr.Character, gameCamera}
+									rayParams.CollisionGroup = root.CollisionGroup
+	
+									local rootSize = root.Size.Y / 2 + entitylib.character.HipHeight
+									if Mode.Value == 'Teleport' then
+										local ray = workspace:Blockcast(root.CFrame, Vector3.new(3, 3, 3), Vector3.new(0, -1000, 0), rayParams)
+										if ray then
+											root.CFrame -= Vector3.new(0, root.Position.Y - (ray.Position.Y + rootSize), 0)
+										end
+									else
+										local ray = workspace:Blockcast(root.CFrame, Vector3.new(3, 3, 3), Vector3.new(0, (tracked * 0.1) - rootSize, 0), rayParams)
+										if ray then
+											tracked = 0
+											root.AssemblyLinearVelocity = Vector3.new(root.AssemblyLinearVelocity.X, -80, root.AssemblyLinearVelocity.Z)
+										end
+									end
+								end
+							end
+						end
+	
+						task.wait(0.03)
+					until not NoFall.Enabled
+				end
+			end
+		end,
+		Tooltip = 'Prevents taking fall damage.'
+	})
+	Mode = NoFall:CreateDropdown({
+		Name = 'Mode',
+		List = {'Packet', 'Gravity', 'Teleport', 'Bounce'},
+		Function = function()
+			if NoFall.Enabled then
+				NoFall:Toggle()
+				NoFall:Toggle()
 			end
 		end
 	})
-
 end)
 
 run(function()
